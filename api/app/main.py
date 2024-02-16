6f9e35fb6ee942187cfc1adc1bce1a53c1e6a344
Quiracle <jsalattorres@gmail.com>
2024-02-16 22:37:11 +0100

    first commit

---
+++ api/app/main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from logging.config import dictConfig
+import logging
+from log_config import LogConfig
+
+from src.User import router as User
+from src.Device import router as Device
+
+from error import error_handler as eh
+from error.AuthenticationException import AuthenticationException
+from error.NotFoundException import NotFoundException
+from error.ValidationException import ValidationException
+from error.InvalidDataException import InvalidDataException
+from error.InputException import InputException
+
+dictConfig(LogConfig().dict())
+logger = logging.getLogger("mycoolapp")
+
+tags_metadata = [
+    {
+        "name": "User",
+        "description": "User related endpoints"
+    },
+    {
+        "name": "Device",
+        "description": "Device related endpoints"
+    },
+]
+
+app = FastAPI(title="LleidaHack API",
+              description="LleidaHack API",
+              version="2.0",
+              docs_url='/docs',
+              redoc_url='/redoc',
+              openapi_url='/openapi.json',
+              openapi_tags=tags_metadata,
+              debug=True,
+              swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+app.add_exception_handler(AuthenticationException,
+                          eh.authentication_exception_handler)
+app.add_exception_handler(NotFoundException, eh.not_found_exception_handler)
+app.add_exception_handler(ValidationException, eh.validation_exception_handler)
+app.add_exception_handler(InvalidDataException,
+                          eh.invalid_data_exception_handler)
+app.add_exception_handler(InputException, eh.input_exception_handler)
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+app.include_router(User.router)
+app.include_router(Device.router)
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(url='/docs')
