391f101d527fbd6a5bc0d6b9a0ae1845ec030a51
Quiracle <jsalattorres@gmail.com>
2024-02-16 23:09:08 +0100

    FEAT: schemas

---
+++ api/app/src/User/schema.py
@@ -0,0 +1,56 @@
+from pydantic import validator
+from datetime import date
+from typing import Optional
+import re
+from utils.BaseSchema import BaseSchema
+
+
+class UserCreate(BaseSchema):
+    name: str
+    surname: str
+    password: str
+    email: str
+    image: Optional[str]
+    is_image_url: Optional[bool]
+
+    @validator('email')
+    def email_validation(cls, v):
+        if (re.search(
+                "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
+                v) is None):
+            raise ValueError('must be a valid email')
+        return v
+
+    @validator('telephone')
+    def telephone_validation(cls, v):
+        if re.search("^([/+][0-9]{1,2})?[0-9]{9}$", v) is None:
+            raise ValueError('must contain at least 8 digits')
+        return v
+
+    @validator('password')
+    def password_validation(cls, v):
+        if (re.search("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$", v)
+                is None):
+            raise ValueError(
+                'must contain at least 8 characters, at least one uppercase letter, one lowercase letter and one number'
+            )
+        return v
+
+
+class UserGet(BaseSchema):
+    name: str
+    surname: str
+    email: str
+    image: Optional[str]
+    is_image_url: Optional[bool]
+
+class UserGetAll(UserGet):
+    password: str
+
+class UserUpdate(BaseSchema):
+    name: Optional[str]
+    surname: Optional[str]
+    password: Optional[str]
+    email: Optional[str]
+    image: Optional[str]
+    is_image_url: Optional[bool]
